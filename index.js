a22931cb01095d5ac4b2663e8d64cc6e72985787
Paul <vmpatoleia@protonmail.com>
2018-09-25 17:01:31 +0000

    check if input is a number

--- index.js
+++ index.js
@@ -9,11 +9,15 @@ bot.on("message", (message) => {
 
     if (message.content.startsWith('!work')) {
         
-         var args = message.content.slice().trim().split(/ +/g);
-         var time = args[1] * 60000
-         console.log(time)
         
-        // 493907904555122698
+         var args = message.content.slice().trim().split(/ +/g)
+         var time = args[1] * 60000
+         
+          if(isNaN(time)) {
+              message.reply(`do you want to be muted forever?`)
+          } else {
+              
+               // 493907904555122698
         message.member.removeRole("493907904555122698").catch(console.error);
         message.reply(`good decision. We will be waiting for your return in ${args[1] || 25} minutes.`)
          setTimeout(
@@ -29,6 +33,8 @@ bot.on("message", (message) => {
         
         
     } 
+              
+          }
    
 })
 
